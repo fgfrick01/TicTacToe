05217c503a69bb3f3cbfe8954d9279c54cdb4c71
Florian Frick <floriangfrick@hotmail.com>
2018-12-30 23:31:03 -0700

    Solved TicTacToe

---
+++ SARSATicTacToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SARSATicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
